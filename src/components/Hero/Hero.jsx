ed8f08b0cc545a33af692bf8519b359eddd98d3a
thejagithub <thejanaravindu2000@gmail.com>
2024-03-21 18:15:50 +0530

    Adding the portfolio files

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+import styles from "./Hero.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Hero = () => {
+  return (
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Hi, I'm Thejana</h1>
+        <p className={styles.description}>
+          I'm an Undergratuate at IIT, with a passion for software development.
+        </p>
+        <a href="mailto:thejanaravindu2000@gmail.com" className={styles.contactBtn}>
+          Contact Me
+        </a>
+      </div>
+      <img
+        src={getImageUrl("hero/heroImage.png")}
+        alt="Hero image of me"
+        className={styles.heroImg}
+      />
+      <div className={styles.topBlur} />
+      <div className={styles.bottomBlur} />
+    </section>
+  );
+};
